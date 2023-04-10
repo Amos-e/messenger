b0141651e62191b425d967ec46ebc6f355b8a2ea
Amos <amosemmanelz@gmail.com>
2023-04-10 14:42:11 +0300

    Changed the asgi config

--- messenger/asgi.py
+++ messenger/asgi.py
@@ -9,12 +9,14 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import main.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
+
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
